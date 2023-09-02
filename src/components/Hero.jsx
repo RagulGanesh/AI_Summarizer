77911ad5a805dca9ee238f23f340741526ebc721
unknown <ragulpretish@gmail.com>
2023-09-02 10:21:44 +0530

    IC

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz_logo" className="w-28 object-contain"/>
-        <button type='button' onClick={()=>{window.open('https://github.com/adrianhajdin/project_ai_summarizer/')}} className="black_btn">
+        <button type='button' onClick={()=>{window.open('https://github.com/RagulGanesh/AI_Summarizer')}} className="black_btn">
           Github
         </button>
       </nav>
